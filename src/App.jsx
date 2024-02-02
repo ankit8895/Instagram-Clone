5d8d4dedbfb984432d9a6d16f0b3a7a7c9e3e804
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2024-02-03 03:47:22 +0530

    implement PageLayout, AuthForm, Sidebar, AuthPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import AuthPage from './pages/AuthPage/AuthPage';
+import PageLayout from './Layouts/PageLayout/PageLayout';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <PageLayout>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/auth' element={<AuthPage />} />
+      </Routes>
+    </PageLayout>
+  );
 };
 
 export default App;
